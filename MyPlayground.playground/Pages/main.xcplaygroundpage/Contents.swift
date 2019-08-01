22e84ed333bbfff02fa9b3deb5bbe81330dc0079
Pat  Pignataro <pat6284@msn.com>
2019-08-01 12:52:38 +0000

    Automatically backed up by Learn

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -35,9 +35,13 @@ declareConst()
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
+func greetings(name: String) {
+    print("Greetings, \(name)!")
+}
 
-
-
+greetings(name: "Bob")
+greetings(name: "Karen")
+greetings(name: "Jerry")
 
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
